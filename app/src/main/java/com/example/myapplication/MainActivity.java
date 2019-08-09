bf050a6d9e7737ddd74c38f287b890b0cc2e74ea
Dilan <vasandarajdilan64@gmail.com>
2019-08-09 18:25:46 +0530

    success

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonOne = findViewById(R.id.button2);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), Report.class);
+                startActivity(activity2Intent);
+            }
+        });
+    }
+
+
+
+
+
+
+    public void send_click(View v)
+    {
+        EditText Name = findViewById(R.id.Name);
+        EditText Email = findViewById(R.id.Email);
+        EditText Subject = findViewById(R.id.Subject);
+        EditText message = findViewById(R.id.message);
+
+        if(Name.getText().toString().equals(""))
+            Name.setError("Mandatory feild");
+        else if (Email.getText().toString().equals(""))
+            Email.setError("Mandatory feild");
+        else if (Subject.getText().toString().equals(""))
+            Subject.setError("Mandatory feild");
+        else if (message.getText().toString().equals(""))
+            message.setError("Mandatory feild");
+        else
+        {
+            Intent i = new Intent(Intent.ACTION_SENDTO);
+            i.setData(Uri.parse("mail to:"));
+            i.putExtra(Intent.EXTRA_EMAIL, new String[] {"vasandarajdilan64@gmail.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, Subject.getText(). toString());
+            i.putExtra(Intent.EXTRA_TEXT,  "dear vasandarajdilan, \n"
+                    + message.getText().toString() + "\n  regards,"
+                    + Email.getText().toString());
+
+            try {
+                startActivity(Intent.createChooser(i,"send mail"));
+            }catch (android.content.ActivityNotFoundException ex)
+            {
+                Toast.makeText(this, "no mail app found ", Toast.LENGTH_SHORT).show();
+            }
+            catch(Exception ex)
+            {
+                Toast.makeText(this ,"unexpected error" + ex.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
+
+    }
+
+
+}
